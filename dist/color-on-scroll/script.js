8f661684eff11ee62c25519af77ffd204470c6a7
Yamen Sharaf <YamenSharaf@Gmail.com>
2017-03-31 18:38:07 +0200

    waypoints added

---
+++ dist/color-on-scroll/script.js
@@ -0,0 +1,50 @@
+var zeroWaypoint = new Waypoint({
+element: document.getElementById('letterhead'),
+handler: function() {
+$('meta[name=theme-color]').remove();
+$('head').append( '<meta name="theme-color" content="#DB649C">' );
+}
+});
+
+
+
+var firstWaypoint = new Waypoint({
+element: document.getElementById('about-me'),
+handler: function() {
+$('meta[name=theme-color]').remove();
+$('head').append( '<meta name="theme-color" content="#EFF0F3">' );
+}
+});
+
+
+var secondWaypoint = new Waypoint({
+element: document.getElementById('skills'),
+handler: function() {
+$('meta[name=theme-color]').remove();
+$('head').append( '<meta name="theme-color" content="#362143">' );
+}
+});
+
+
+
+var thirdWaypoint = new Waypoint({
+element: document.getElementById('portfolio'),
+handler: function() {
+$('meta[name=theme-color]').remove();
+$('head').append( '<meta name="theme-color" content="#DB649C">' );
+}
+});
+
+var fourthWaypoint = new Waypoint({
+element: document.getElementById('blog'),
+handler: function() {
+$('meta[name=theme-color]').remove();
+$('head').append( '<meta name="theme-color" content="#403049">' );
+}
+});
+
+
+/* {
+$('meta[name=theme-color]').remove();
+$('head').append( '<meta name="theme-color" content="#8BC34A">' );
+}*/
