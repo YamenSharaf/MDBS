c93dcda71157d878cb1a300651d7851be2e0daf8
Yamen Sharaf <YamenSharaf@Gmail.com>
2017-05-07 21:28:21 +0200

    everything works

--- code.js
+++ code.js
@@ -3,10 +3,10 @@ axios.get('http://yamensharaf.tk/blog/?json=get_recent_posts')
     const postsDiv = document.getElementById('blogPosts');
     let posts = [];
     posts = response.data.posts;
-    for (var i = 0; i < posts.length; i++) {
+    for (var i = 0; i < 6; i++) {
       let title = posts[i].title;
-      let thumbnail = posts[i].thumbnail_images.medium.url;
-      console.log(thumbnail);
+      let thumbnail = posts[i].thumbnail_images.medium_large.url;
+      let link = posts[i].url;
       let card = `
       <div class="col-md-4 bottom-buffer">
           <!--Card-->
@@ -14,17 +14,18 @@ axios.get('http://yamensharaf.tk/blog/?json=get_recent_posts')
               <!--Card image-->
               <div class="view overlay hm-white-slight">
                   <img src=${thumbnail} class="img-fluid" alt="WordPress blog coming soon">
-                  <a>
+                  <a target="_blank" href=${link}>
                       <div class="mask waves-effect waves-light"></div>
                   </a>
               </div>
               <!--Card content-->
               <div class="card-block">
-                  <h5 class="card-text"><i class="mdi mdi-wordpress"></i> ${title}</h5>
+                  <a target="_blank" href = ${link}><h5 class="card-text"><i class="mdi mdi-bookmark-outline"></i> ${title}</h5></a>
               </div>
               <!--/.Card-->
           </div>
       </div>
+
       `
       postsDiv.insertAdjacentHTML('beforeend', card);
     }
@@ -32,23 +33,3 @@ axios.get('http://yamensharaf.tk/blog/?json=get_recent_posts')
   .catch(function (error) {
     console.log(error);
   });
-
-
-
-let content = `<div class="col-md-4 bottom-buffer">
-    <!--Card-->
-    <div data-aos="fade-up" class="card card-cascade narrower top-buffer">
-        <!--Card image-->
-        <div class="view overlay hm-white-slight">
-            <img src="img/wp.jpg" class="img-fluid" alt="WordPress blog coming soon">
-            <a>
-                <div class="mask waves-effect waves-light"></div>
-            </a>
-        </div>
-        <!--Card content-->
-        <div class="card-block">
-            <h5 class="card-text"><i class="mdi mdi-wordpress"></i> Powered by WordPress</h5>
-        </div>
-        <!--/.Card-->
-    </div>
-</div>`
